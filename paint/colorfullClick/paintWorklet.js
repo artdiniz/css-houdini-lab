cfb7fec4b59a425260c135a28e09d22ad51bdbf6
Artur Diniz Adam <artur.daz@gmail.com>
2017-07-13 00:49:24 -0300

    better interface for developers

--- paint/colorfullClick/paintWorklet.js
+++ paint/colorfullClick/paintWorklet.js
@@ -1,8 +1,17 @@
 const reduceColor = (color, percentage = 1) => parseInt(color * Math.max(Math.min(1,percentage), 0))
 
-const rgbColorStringToObject = (string) => {
-    const {1:red, 2:green, 3:blue} = string.trim().match(/^rgb\(([1-2]*[1-9]*[1-9]+)\s*,\s*([1-2]*[1-9]*[1-9]+)\s*,\s*([1-2]*[1-9]*[1-9]+)\)$/)
-    return {red: parseInt(red), green: parseInt(green), blue: parseInt(blue)}
+const colorStringToObject = (string) => {
+    string = string.toUpperCase()
+    const rgbColor = string.trim().match(/^rgb\(([1-2]*[1-9]*[1-9]+)\s*,\s*([1-2]*[1-9]*[1-9]+)\s*,\s*([1-2]*[1-9]*[1-9]+)\)$/i)
+    const hexaColor = string.trim().match(/^#([0-F]{2})([0-F]{2})([0-F]{2})$/i)
+    
+    const {1:red, 2:green, 3:blue} = rgbColor || hexaColor || [null, 255, 255, 255]
+
+    if(rgbColor){
+        return {red: parseInt(red), green: parseInt(green), blue: parseInt(blue)}
+    } else {
+       return  {red: parseInt(red, 16), green: parseInt(green, 16), blue: parseInt(blue, 16)}
+    }
 }
 
 const objectToRGBString = (colorObject) => `rgb(${colorObject.red}, ${colorObject.green}, ${colorObject.blue})`
@@ -16,22 +25,15 @@ const diffColors = (color1, color2) => ({
 const msStringToInt = (string) => parseInt(string.replace(/ms$/, ""))
 
 registerPaint('colorfull-click', class {
-    static get inputProperties() { return ['background-color', '--colorfull-click-x', '--colorfull-click-y', '--colorfull-click-animation-tick', '--colorfull-click-animation-duration'] }
+    static get inputProperties() { return ['background-color', '--colorfull-click-color', '--colorfull-click-animation-tick', '--colorfull-click-animation-duration'] }
     paint(ctx, geom, props){
-        const xPercentage = parseFloat(props.get('--colorfull-click-x').toString()) / geom.width
-        const yPercentage = parseFloat(props.get('--colorfull-click-y').toString()) / geom.height
         
         const duration = msStringToInt(props.get('--colorfull-click-animation-duration').toString())
         const tick = Math.min(duration, Math.max(0, parseFloat(props.get('--colorfull-click-animation-tick').toString())))      
         const animationPercentage = Math.abs(Math.abs(tick - (duration/2)) / (duration/2) - 1)
 
-        const bgColor = rgbColorStringToObject(props.get('background-color').toString())
-
-        const objectiveColor = {
-            red: reduceColor(255, xPercentage)
-            ,green: reduceColor(255, yPercentage)
-            ,blue: reduceColor(255, (xPercentage / yPercentage)%1)
-        }
+        const bgColor = colorStringToObject(props.get('background-color').toString())
+        const objectiveColor = colorStringToObject(props.get('--colorfull-click-color').toString()) 
 
         const colorDelta = diffColors(bgColor, objectiveColor)
 
@@ -40,12 +42,6 @@ registerPaint('colorfull-click', class {
             ,green: bgColor.green + reduceColor(colorDelta.green, animationPercentage)
             ,blue: bgColor.blue + reduceColor(colorDelta.blue, animationPercentage)
         }
-
-        console.log("Original: ", objectToRGBString(bgColor))
-        console.log("Objective: ", objectToRGBString(objectiveColor))
-        console.log("Delta: ", objectToRGBString(colorDelta))
-        console.log("Current: ", objectToRGBString(currentColor))
-        
 
         ctx.fillStyle = objectToRGBString(currentColor)
         ctx.fillRect(0, 0, geom.width, geom.height);
